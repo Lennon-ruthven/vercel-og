ab0b9026ce7faff20a007fb4764cca467d6b66f0
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-30 03:06:10 +0100

    Add files via upload

--- src/components/layouts/FooterComponent.tsx
+++ src/components/layouts/FooterComponent.tsx
@@ -4,12 +4,12 @@ const FooterComponent = () => {
   return (
     <footer className="bottom-0 py-16 text-center">
       <a
-        href="https://yehezgun.com"
+        href="https://vercel-og-gamma.vercel.app/"
         target="_blank"
         className="cursor-newtab font-semibold hover:underline"
         rel="noreferrer"
       >
-        {new Date().getFullYear()} | Yehezkiel Gunawan
+        {new Date().getFullYear()} | Lennon Ruthven
       </a>
     </footer>
   );
